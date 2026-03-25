d6e0b63e89d971174ba6f525a40789c9cb79990a
sasuns0 <sasunhakobyan2002@gmail.com>
2026-03-25 18:04:25 +0400

    db fix for supabase

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgresql://localhost/sasblog',
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
