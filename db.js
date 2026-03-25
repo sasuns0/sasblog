5a265245cd3485a119c335bc00715e8447941cc9
sasuns0 <sasunhakobyan2002@gmail.com>
2026-03-25 21:47:02 +0400

    little fix

--- db.js
+++ db.js
@@ -2,7 +2,6 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL || 'postgresql://localhost/sasblog',
-  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
